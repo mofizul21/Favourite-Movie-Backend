b66fec686d913ff6f6d8ed01d446137ff52a8e08
Mofizul <mofizul21@gmail.com>
2023-01-30 18:38:16 +0600

    Update port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api'); // http://localhost:3000/api/
-  await app.listen(process.env.APP_LISTEN_PORT || 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
